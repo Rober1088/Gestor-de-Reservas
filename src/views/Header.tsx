e2da8a846da94b4e325fc3ab299c249ced7b71db
moll1nedo <eduardo1mollinedo@gmail.com>
2025-05-05 16:56:14 -0600

    Comit hader

---
+++ src/views/Header.tsx
@@ -0,0 +1,13 @@
+// src/components/Header.tsx
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Gestor de Reservas</h1>
+    </header>
+  );
+};
+
+export default Header;
